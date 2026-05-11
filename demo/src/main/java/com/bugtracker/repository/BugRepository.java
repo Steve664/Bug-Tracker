1a6a8d051eba1706d9b00ebcc1cde30d1a8d769e
Steve664 <stevekariuki5@gmail.com>
2026-05-11 07:43:44 +0300

    feat: Updated the Spring Data repositories so Bug, User, and Comment all extend JpaRepository

--- demo/src/main/java/com/bugtracker/repository/BugRepository.java
+++ demo/src/main/java/com/bugtracker/repository/BugRepository.java
@@ -1,7 +1,14 @@
 package com.bugtracker.repository;
 
 import com.bugtracker.model.Bug;
+import com.bugtracker.model.Severity;
+import com.bugtracker.model.Status;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BugRepository extends JpaRepository<Bug, Long> {
+
+    List<Bug> findByStatus(Status status);
+
+    List<Bug> findBySeverity(Severity severity);
 }
